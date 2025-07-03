80e907c8447d63b05403c52f6bcdd17d0ee49aff
R.E.N.E.G.A.N. <shy.kyrinay100@gmail.com>
2025-07-04 00:18:48 +0700

    добавил еду

---
+++ dish.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RestaurantApp
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
