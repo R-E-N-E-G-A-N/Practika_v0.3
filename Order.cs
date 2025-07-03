c6c5f1fd82ae05c8db00c86974a414858df5abf5
R.E.N.E.G.A.N. <shy.kyrinay100@gmail.com>
2025-07-04 01:41:58 +0700

    добавил Order.cs и Person.cs

---
+++ Order.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RestaurantApp
+{
+    public enum OrderStatus { Created, Paid }
+
+    public class Order
+    {
+        public string Id { get; private set; }
+        public List<DishQuantity> Dishes { get; set; }
+        public OrderStatus Status { get; set; }
+        public string ClientId { get; set; }
+        public string WaiterId { get; set; }
+        public double TotalPrice
+        {
+            get
+            {
+                double total = 0;
+                foreach (var dishQty in Dishes)
+                {
+                    total += dishQty.Dish.Price * dishQty.Quantity;
+                }
+                return total;
+            }
+        }
+
+        public Order()
+        {
+            Id = Guid.NewGuid().ToString();
+            Dishes = new List<DishQuantity>();
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RestaurantApp
+{
+    public enum Role { Client, Waiter }
+
+    public class Person
+    {
+        public string Id { get; private set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public Role Role { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public Person()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
